6d6286345c70307d27598d4d4ce7119b9d6f1a89
hongweimachene <hchen00@stuy.edu>
2019-03-13 11:44:54 -0400

    started quicksort

--- Quick.java
+++ Quick.java
@@ -1,4 +1,6 @@
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Quick{
   public static int quickSelect(int[] data, int k) {
@@ -15,6 +17,7 @@ public class Quick{
     kth = data[val];
     return kth;
   }
+
   /*Modify the array such that:
  *1. Only the indices from start to end inclusive are considered in range
  *2. A random index from start to end inclusive is chosen, the corresponding
@@ -70,15 +73,19 @@ public class Quick{
    quickSort(data, lo, pivot - 1);
    quickSort(data, pivot + 1, hi);
  }
- 
+
  public static void main(String[] args) {
    int[] ary1 = new int[] {8, 6 , 7 , 5, 3 ,0, 9};
-   System.out.println(partition(ary1, 0, ary1.length-1));
+   // System.out.println(partition(ary1, 0, ary1.length-1));
+   // for (int i = 0; i < ary1.length; i++) {
+   //   System.out.print(ary1[i]+ " ");
+   // }
+   // System.out.println(" ");
+   // int[] ary = new int[] { 2, 10, 15, 23, 0,  5};
+   // System.out.println(quickSelect(ary, 2));
+   quickSort(ary1);
    for (int i = 0; i < ary1.length; i++) {
      System.out.print(ary1[i]+ " ");
    }
-   System.out.println(" ");
-   int[] ary = new int[] { 2, 10, 15, 23, 0,  5};
-   System.out.println(quickSelect(ary, 2));
  }
 }
