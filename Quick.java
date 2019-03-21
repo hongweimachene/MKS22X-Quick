61ba930b3c9e669af09607e9af37cae03cbd4d3a
hongweimachene <hchen00@stuy.edu>
2019-03-21 00:50:41 -0400

    added insertionsort improvement

--- Quick.java
+++ Quick.java
@@ -17,6 +17,25 @@ public class Quick{
     return kth;
   }
 
+  public static void insertionSort(int[] ary, int lo, int hi) {
+   //the sort starts at index 1 because it is the sorted side
+   for (int i = 1; i < ary.length; i++) {
+     //unsorted is being compared to each value in the sorted side
+     int unsorted = ary[i];
+     int sorted = i - 1;
+     //checks if sorted side is greater than the unsorted value
+     while (sorted >= 0 && ary[sorted] > unsorted ) {
+       //shifts each value forward to the right if the sorted is greater
+       ary[sorted+1] = ary[sorted];
+       sorted--;
+     }
+     //if the while loop is done, means sorted side is sorted and insert the unsorted
+     //into where it belongs,
+     //the right of the next smallest number
+     ary[sorted+1] = unsorted;
+   }
+ }
+
   /*Modify the array such that:
  *1. Only the indices from start to end inclusive are considered in range
  *2. A random index from start to end inclusive is chosen, the corresponding
@@ -79,7 +98,7 @@ public class Quick{
    if (pivot < data[start]) {
      start--;
    }
-   //swap pivot with start 
+   //swap pivot with start
    data[pindex] = data[start];
    data[start] = pivot;
    index = start;
@@ -97,47 +116,18 @@ public class Quick{
    if (lo >= hi) {
      return;
    }
+   if (data.length <= 35) {
+     insertionSort(data,lo,hi);
+   } else {
    //initial pivot
    int pivot = partition(data, lo, hi);
    //for log(n) runtime, split array in half each time to partition
    quicksort(data, lo, pivot - 1);
    quicksort(data, pivot + 1, hi);
  }
+ }
 
- public static void main(String[]args){
-  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
-  int[]MAX_LIST = {1000000000,500,10};
-  for(int MAX : MAX_LIST){
-    for(int size = 31250; size < 2000001; size*=2){
-      long qtime=0;
-      long btime=0;
-      //average of 5 sorts.
-      for(int trial = 0 ; trial <=5; trial++){
-        int []data1 = new int[size];
-        int []data2 = new int[size];
-        for(int i = 0; i < data1.length; i++){
-          data1[i] = (int)(Math.random()*MAX);
-          data2[i] = data1[i];
-        }
-        long t1,t2;
-        t1 = System.currentTimeMillis();
-        Quick.quicksort(data2);
-        t2 = System.currentTimeMillis();
-        qtime += t2 - t1;
-        t1 = System.currentTimeMillis();
-        Arrays.sort(data1);
-        t2 = System.currentTimeMillis();
-        btime+= t2 - t1;
-        if(!Arrays.equals(data1,data2)){
-          System.out.println("FAIL TO SORT!");
-          System.exit(0);
-        }
-      }
-      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
-    }
-    System.out.println();
-  }
-}
+
 
 
 }
